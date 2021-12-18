b02ee371c167800af4d364650aac32afebaefa3c
Byzic <lbyzova2001@yandex.ru>
2021-12-18 03:36:34 +0300

    jta

--- src/main/java/org/lera/lab3/repository/PointRepository.java
+++ src/main/java/org/lera/lab3/repository/PointRepository.java
@@ -1,5 +1,8 @@
 package org.lera.lab3.repository;
-
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.*;
 import org.lera.lab3.model.Point;
 
 import javax.enterprise.context.SessionScoped;
@@ -14,22 +17,37 @@ import java.util.List;
 @Named
 @SessionScoped
 public class PointRepository implements Serializable {
+    private static final String PERSISTENCE_UNIT_NAME = "data";
+    @PersistenceContext(unitName = PERSISTENCE_UNIT_NAME)
+    private EntityManager entityManager;
+
+    @Resource
+    private UserTransaction userTransaction;
 
     public List<Point> getPoints(String userId) {
-        EntityManager entityManager = Persistence.createEntityManagerFactory("data").createEntityManager();
-        Query query = entityManager.createQuery(
-                "SELECT c FROM Point c WHERE c.userId LIKE :userId")
-                .setParameter("userId", userId);
-        List<Point> items = query.getResultList();
-        Collections.reverse(items);
-        return items;
+        return entityManager.createQuery("SELECT c FROM Point c WHERE c.userId LIKE :userId").getResultList();
+//        EntityManager entityManager = Persistence.createEntityManagerFactory("data").createEntityManager();
+//        Query query = entityManager.createQuery(
+//                "SELECT c FROM Point c WHERE c.userId LIKE :userId")
+//                .setParameter("userId", userId);
+//        List<Point> items = query.getResultList();
+//        Collections.reverse(items);
+//        return items;
     }
 
     public void addPoint(Point point) {
-        EntityManager entityManager = Persistence.createEntityManagerFactory("data").createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.persist(point);
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        try {
+            userTransaction.begin();
+            entityManager.persist(point);
+            userTransaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        EntityManager entityManager = Persistence.createEntityManagerFactory("data").createEntityManager();
+//        entityManager.getTransaction().begin();
+//        entityManager.persist(point);
+//        entityManager.getTransaction().commit();
+//        entityManager.close();
     }
 }
